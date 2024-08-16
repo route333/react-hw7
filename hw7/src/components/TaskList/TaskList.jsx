66f16e57e51a47fc3ee04ecc4993d1a9443b5bf3
Oleg Bondarenko <109847185+route333@users.noreply.github.com>
2024-08-16 14:59:27 +0300

    hw

--- hw7/src/components/TaskList/TaskList.jsx
+++ hw7/src/components/TaskList/TaskList.jsx
@@ -1,25 +1,66 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
 
-class TaskList extends Component{
-    tasks = [
-        {id:1, text:"Task 1"},
-        {id:2, text:"Task 2"},
-        {id:3, text:"Task 3"},
-        ];
-    render(){
-        const {id, text} =this.tasks;
-        return (
-            <div>
-              {this.tasks.map((task) => (
-                <div key={task.id}>
-                  <p>{task.id}</p>
-                  <p>{task.text}</p>
-                  <button onClick={event => }>Click me!</button>
-                </div>
-              ))}
-            </div>
+const TasksContainer = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 25px;
+  justify-content: center;
+  list-style: none;
+`;
+
+const Task = styled.li`
+  background-color: grey;
+  border-radius: 5%;
+  width: 200px;
+  height: 200px;
+`;
+
+const TaskText = styled.p`
+  color: white;
+`;
+
+const DelTaskButton = styled.button`
+  background-color: white;
+  color: black;
+  padding: 10px 10px 10px;
+  border-radius: 7%;
+  border: none;
+`;
+
+class TaskList extends Component {
+  state = {
+    tasks: [
+      { id: 1, text: "Task 1" },
+      { id: 2, text: "Task 2" },
+      { id: 3, text: "Task 3" },
+      { id: 4, text: "Task 4" },
+      { id: 5, text: "Task 5" },
+      { id: 6, text: "Task 6" },
+      { id: 7, text: "Task 7" },
+    ],
+  };
+
+  Delete = (id) => {
+    const updateTasks = this.state.tasks.filter((task) => task.id !== id);
+    this.setState({ tasks: updateTasks });
+  };
+
+  render() {
+    return (
+      <TasksContainer>
+        {this.state.tasks.map((task) => (
+          <Task key={task.id}>
+            <TaskText>{task.id}</TaskText>
+            <TaskText>{task.text}</TaskText>
+            <DelTaskButton onClick={() => this.Delete(task.id)}>
+              Видалити
+            </DelTaskButton>
+          </Task>
+        ))}
+      </TasksContainer>
     );
-}
+  }
 }
 
 export default TaskList;
